2ddcf100e8c2b45c980a9f8ed9d1fc4af320124e
Dominik Horb <git@dominik.berlin>
2017-05-02 23:42:32 +0200

    Merge package changes

--- bin/brew-setup.sh
+++ bin/brew-setup.sh
@@ -6,12 +6,11 @@
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -20,11 +19,15 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -58,6 +61,7 @@ brew install phpmd
 brew install phpunit
 brew install ssh-copy-id
 brew install symfony-installer
+brew install testssl
 brew install yarn
 
 # Remove outdated versions from the cellar.
